7fc91ef4c675bedd26962d9d06a860eca394308c
TheLost <the_losts@163.com>
2016-04-15 11:03:04 +0800

    SendMail

---
+++ config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author TheLost
+
+mail_host = ""		        #设置服务器
+mail_user = ""			#用户名
+mail_pass = ""		        #口令 
+mail_postfix = ""		#发件箱类型
---
+++ send_Email.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author TheLost
+
+'''
+发送简单文本邮件
+'''
+import smtplib
+import config
+from email.mime.text import MIMEText
+
+class SendEmail:
+
+    def send_Emails(self, from_name, to_list, sub, content):
+        me = from_name + '<' + config.mail_user + '@' + config.mail_postfix + '.com' + '>'
+        msg = MIMEText(content, _subtype = 'plain', _charset = 'utf-8')
+        msg['Subject'] = sub
+        msg['From'] = me
+        msg['To'] = ";".join(to_list)
+
+        try:
+            server = smtplib.SMTP()
+            server.connect(config.mail_host)
+            server.login(config.mail_user, config.mail_pass)
+            server.sendmail(me, to_list, msg.as_string())
+            server.close()
+            return True
+        except Exception, e:
+            print str(e)
+            return False
+
+if __name__ == '__main__':
+
+    mailto_list = ['hacker_email@qq.com']
+    result = SendEmail().send_Emails('Hacker', mailto_list, 'Send to Hacker', '老板，来活了！！！')
